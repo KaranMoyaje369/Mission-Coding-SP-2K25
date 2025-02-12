2e63705d199c98d3fb37811ab8e548095db650d9
KaranMoyaje369 <karanmoyaje369@gmail.com>
2025-02-13 02:15:52 +0530

    Mission Coding

---
+++ MC-Que-4/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-center text-orange-500">Project 4</h1>
+    </div>
+  );
+};
+
+export default App;
